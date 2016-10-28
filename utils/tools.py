eef99894da93f618aa5e544c30b413c810b01734
Benoît Porteboeuf <porteboeuf.benoit@gmail.com>
2016-10-28 16:10:57 +0200

    minor bug fixed in getInput

--- utils/tools.py
+++ utils/tools.py
@@ -99,7 +99,7 @@ def getInput(message): # Message is a message to display
     else :
         string = input(str(message))
 
-    if re.search(r"^[0-9A-Za-z-_. \/]{1,60}$", string) is None :
+    if re.search(r"^[0-9A-Za-z-_. \/]{0,60}$", string) is None :
         raise NameError('PhotoWizard Error: Unexpected input')
         return
     else :
