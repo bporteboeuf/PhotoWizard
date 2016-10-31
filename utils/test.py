c9d95dec71464f47cf1a55e88cb28e2a9e38b4de
Benoît Porteboeuf <porteboeuf.benoit@gmail.com>
2016-10-31 21:38:49 +0100

    Test file added

---
+++ utils/test.py
@@ -0,0 +1,62 @@
+#/////////////////////////////#
+# - P H O T O   W I Z A R D - #
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
+
+# -*- coding: utf-8 -*-
+
+# This module contains tests for the whole software
+
+
+import filters,helpm,history,levels,main,tools
+from config import *
+import numpy,scipy,math
+from PIL import Image
+
+
+###***--- THE PICTURE CLASS ---***###
+
+
+
+###***--- THE HISTORY CLASS ---***###
+
+
+
+###***--- THE FILTERS FILE ---***###
+
+
+
+###***--- THE HELPM FILE ---***###
+
+
+
+###***--- THE HISTORY FILE ---***###
+
+
+
+###***--- THE LEVELS FILE ---***###
+
+
+
+###***--- THE MAIN FILE ---***###
+
+
+
+###***--- THE TOOLS FILE ---***###
+
+
+
+###***--- A FEW USERCASES ---***###
+
+
+## UserCase1
+
+
+## UserCase2
+
+
+## UserCase3
+
+
+
+
+
