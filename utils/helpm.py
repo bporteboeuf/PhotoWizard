787666483a1e8ba4a8c1667913de12872b9fa2f5
Benoît Porteboeuf <porteboeuf.benoit@gmail.com>
2016-11-06 16:55:58 +0100

    French help version implemented

--- utils/helpm.py
+++ utils/helpm.py
@@ -80,11 +80,11 @@ def help(matter,language): # Matter indicates the matter on which the user needs
 
         elif matter == 'contrast':
             hmess = '\nHELP - CONTRAST'
-            hmess += '\nUnfortunately, this function is not implemented yet. Try using levels or curves.'
+            hmess += '\nUnfortunately, this function is not implemented yet. Try using LEVELS or CURVES.'
 
         elif matter == 'exposure':
             hmess = '\nHELP - EXPOSURE'
-            hmess += '\nUnfortunately, this function is not implemented yet. Try using levels or curves.'
+            hmess += '\nUnfortunately, this function is not implemented yet. Try using LEVELS or CURVES.'
 
         elif matter == 'histogram':
             hmess = '\nHELP - HISTOGRAM'
@@ -139,7 +139,7 @@ def help(matter,language): # Matter indicates the matter on which the user needs
 
         elif matter == 'blackandwhite':
             hmess = '\nHELP - BLACKANDWHITE'
-            hmess += '\nUnfortunately, this function is not implemented yet. Try using levels on the S channel.'
+            hmess += '\nUnfortunately, this function is not implemented yet. Try using LEVELS on the S channel.'
 
         elif matter == 'rotate':
             hmess = '\nHELP - ROTATE'
@@ -180,7 +180,7 @@ def help(matter,language): # Matter indicates the matter on which the user needs
         elif matter == 'redo':
             hmess = '\nHELP - REDO'
             hmess += '\nThis command redoes or restore your last cancelled action by the UNDO command. If you want to restore more than one action, it will try to go along your history branch as long as it does not find a leaf or a node.'
-            hmess += '\n\nNote: If a node is found and the redo action proves to be inefficient, you can always force it to enter a new branch or to directly restore a new state by using the REBASE command.'
+            hmess += '\n\nNote: If a node is found and the REDO action proves to be inefficient, you can always force it to enter a new branch or to directly restore a new state by using the REBASE command.'
             hmess += '\n\nExample: redo'
             hmess += '\n'
 
@@ -209,7 +209,7 @@ def help(matter,language): # Matter indicates the matter on which the user needs
         elif matter == 'load':
             hmess = '\nHELP - LOAD'
             hmess += '\nThis command loads an XMD file which contains a history of modifications, forks it to your current history and update your current state.'
-            hmess += '\nYou just need to specify the path of the XMD file (relative or absolute)'
+            hmess += '\nYou just need to specify the path of the XMD file (relative or absolute).'
             hmess += '\n\nExample: load pic/test3.xmd'
             hmess += '\n'
 
@@ -221,14 +221,14 @@ def help(matter,language): # Matter indicates the matter on which the user needs
 
         elif matter == 'export':
             hmess = '\nHELP - EXPORT'
-            hmess += '\nThis command actually exports your final image. All your modifications will then be applied to your original full-sized image.'
+            hmess += '\nThis command actually exports your final image. All your modifications will then be applied to your full-sized image.'
             hmess += '\nYou just need to specify the desired path (relative or absolute). The extension of the file will be interpreted as the format for export.'
             hmess += '\n\nExample: export pic/test3.tiff'
             hmess += '\n'
 
         elif matter == 'switch':
             hmess = '\nHELP - SWITCH'
-            hmess += '\nThis function allows you to switch between several opened images so that you can edit them in parallel.'
+            hmess += '\nThis function allows you to switch between several opened images so that you can edit different pictures in parallel.'
             hmess += '\nYou just need to specify the ID of your picture, which is the path you have entered to open it in the first place.'
             hmess += '\n\nExample: switch pic/test2.jpg'
             hmess += '\n'
@@ -254,87 +254,208 @@ def help(matter,language): # Matter indicates the matter on which the user needs
     
         elif matter == 'levels':
             hmess = '\nAIDE - LEVELS'
-
+            hmess += '\nCe module offre la possibilité de modifier les niveaux d\'une image, c\'est-à-dire de modifier les valeurs de sorties sur des intervalles d\'entrée donnés.'
+            hmess += '\nIl faut tout d\'abord sélectionner un canal sur lequel appliquer l\'effet, au choix parmi R,G,B,H,S,V ou ALL.'
+            hmess += '\nIl faut ensuite donner les bornes des intervalles d\'entrée comme suit : [ia,ib,ic] où (ia,ib,ic) sont des entiers de 0 à 255, ia/ib définissant les tons sombres de l\'image et ib/ic les tons clairs.'
+            hmess += '\nDe la même manière, il faut renseigner les bornes des intervalles de sortie [oa,ob,oc] comme des entiers de 0 à 255.'
+            hmess += '\n\nRemarque : il est possible de renseigner autant de bornes en entrée et en sortie, du moment que les longueurs des listes d\'entrées et de sorties sont égales et comprises entre 2 et 257.'
+            hmess += '\n\nExemple : levels ALL [30,128,220] [0,128,255]'
+            hmess += '\n'
+        
         elif matter == 'curves':
             hmess = '\nAIDE - CURVES'
+            hmess += '\nCe module offre la possibilité de changer la courbe de dynamique de l\'image, c\'est-à-dire de changer les valeurs de sortie en fonction d\'intervalles de valeurs d\'entrée précisés.'
+            hmess += '\nCe module fonctionne de manière très similaire au module LEVELS, sauf que LEVELS propose une réponse dynamique C-0 alors que CURVES propose une réponse dynamique C1 en utilisant une interpolation par spline.'
+            hmess += '\nIl faut tout d\'abord sélectionner un cannal sur lequel appliquer l\'effet, au choix parmi R,G,B,H,S,V ou ALL.'
+            hmess += '\nIl faut ensuite donner les bornes des intervalles d\'entrée comme suit : [ia,ib,ic] où (ia,ib,ic) sont des entiers de 0 à 255, ia/ib définissant les tons sombres de l\'image et ib/ic les tons clairs.'
+            hmess += '\nDe la même manière, il faut renseigner les bornes des intervalles de sortie [oa,ob,oc] comme des entiers de 0 à 255.'
+            hmess += '\n\nRemarque : il est possible de renseigner autant de bornes en entrée et en sortie, du moment que les longueurs des listes d\'entrées et de sorties sont égales et comprises entre 4 et 257.'
+            hmess += '\n\nExemple : curves ALL [20,30,50,128,200,220,230] [0,5,15,128,240,250,255]'
+            hmess += '\n'
 
         elif matter == 'eqHist':
             hmess = '\nAIDE - EQHIST'
+            hmess += '\nCe module offre la possibilité d\'égaliser l\'histogramme, ce qui est une moyen d\'augmenter le contrast et le détail global d\'une image.'
+            hmess += '\nIl suffit de sélectionner un cannal sur lequel appliquer l\'effet, au choix parmi R,G,B,H,S,V ou ALL.'
+            hmess += '\n\nExemple : eqHist V'
+            hmess += '\n'
 
         elif matter == 'normHist':
             hmess = '\nAIDE - NORMHIST'
+            hmess += '\nCe module offre la possibilité de normaliser l\'histogramme en utilisant une fonction linéaire, ce qui est un moyen d\'augmenter le contraste et le détail global d\'une image.'
+            hmess += '\nIl suffit de sélectionner un cannal sur lequel appliquer l\'effet, au choix parmi R,G,B,H,S,V ou ALL.'
+            hmess += '\n\nExemple : normHist V'
+            hmess += '\n'
 
         elif matter == 'logHist':
             hmess = '\nAIDE - LOGHIST'
+            hmess += '\nCe module offre la possibilité de normaliser l\'histogramme en utilisant une fonction logarithmique, ce qui est un moyen d\'augmenter le contraste et le détail global d\'une image sous-exposée, en restaurant beaucoup de détails dans les zones sombres.'
+            hmess += '\nIl suffit de sélectionner un cannal sur lequel appliquer l\'effet, au choix parmi R,G,B,H,S,V ou ALL.'
+            hmess += '\n\nExemple : logHist V'
+            hmess += '\n'
 
-        elif matter == 'expHist':
+        elif matter == '\nexpHist':
             hmess = '\nAIDE - EXPHIST'
+            hmess += '\nCe module offre la possibilité de normaliser l\'histogramme en utilisant une fonction exponentielle, ce qui est un moyen d\'augmenter le contraste et le détail global d\'une image sur-exposée, en restaurant beaucoup de détails dans les zones claires.'
+            hmess += '\nIl suffit de sélectionner un cannal sur lequel appliquer l\'effet, au choix parmi R,G,B,H,S,V ou ALL.'
+            hmess += '\n\nExemple : expHist V'
+            hmess += '\n'
 
         elif matter == 'contrast':
             hmess = '\nAIDE - CONTRAST'
+            hmess += '\nMalheureusement, cette fonction n\'est pas encore implémentée. Essayez d\'utiliser le module LEVELS ou CURVES.'
 
         elif matter == 'exposure':
             hmess = '\nAIDE - EXPOSURE'
+            hmess += '\nMalheureusement, cette fonction n\'est pas encore implémentée. Essayez d\'utiliser le module LEVELS ou CURVES.'
 
         elif matter == 'histogram':
             hmess = '\nAIDE - HISTOGRAM'
+            hmess += '\nCe module affiche un histogramme normalisé de la copie de travail de votre image.'
+            hmess += '\nUn histogramme est un diagramme qui explique quel nombre de pixels ont une valeur donnée. Les valeurs (entiers de 0 à 255 de gauche à droite) sont sur l\'axe des abscisses et le nombre (ou la proportion) de pixels correspondants représenté sur l\'axe des ordonnées.'
+            hmess += '\nC\'est par exemple un très bon outil pour savoir si une image est sous-exposée ou sur-exposée.'
+            hmess += '\nIl suffit de sélectionner un cannal sur lequel appliquer l\'effet, au choix parmi R,G,B,H,S,V ou ALL.'
+            hmess += '\n\nExemple : histogram R'
+            hmess += '\n'
 
         elif matter == 'lowPass':
             hmess = '\nAIDE - LOWPASS'
+            hmess += '\nCe module offre la possibilité d\'appliquer un filtre passe-bas sur une image (soit essentiellement un effet de flou).'
+            hmess += '\nIl faut d\'abord sélectionner le type de filtre à utiliser. Pour l\'instant, vous pouvez choisir parmi : GAUSSIAN-2D, MEAN-2D ou POISSON-2D.'
+            hmess += '\nIl faut ensuite fournir les paramètres nécessaires au filtre, comme le rayon (entier) et éventuellement un coefficient (flottant) pour GAUSSIAN-2D et POISSON-2D.'
+            hmess += '\nEnfin, il faut préciser le canal sur lequel appliquer l\'effet, au choix parmi R,G,B,H,S,V ou ALL.'
+            hmess += '\n\nExemple : lowPass GAUSSIAN-2D [20,1] ALL'
+            hmess += '\n'
 
         elif matter == 'highPass':
-            hmess = '\nAIDE - HIGHPASS'
+            hmess = '\nAIDE- HIGHPASS'
+            hmess += '\nCe module offre la possibilité d\'appliquer un filtre passe-haut sur une image (soit essentiellement un effet d\'accentuation).'
+            hmess += '\nIl faut d\'abord sélectionner le type de filtre à utiliser. Pour l\'instant, vous pouvez choisir parmi : DIFF-2D.'
+            hmess += '\nIl faut ensuite fournir les paramètres nécessaires au filtre, comme le rayon (entier).'
+            hmess += '\nEnfin, il faut préciser le canal sur lequel appliquer l\'effet, au choix parmi R,G,B,H,S,V ou ALL.'
+            hmess += '\n\nExemple : highPass DIFF-2D [5]  ALL'
+            hmess += '\n'
 
         elif matter == 'detectEdges':
             hmess = '\nAIDE - DETECTEDGES'
+            hmess += '\nCe module offre la possibilité de détecter les contours sur une image.'
+            hmess += '\nCe module fonctionne de manière similaire au module HIGHPASS sauf qu\'il nécessite également une valeur de seuil.'
+            hmess += '\nIl faut d\'abord sélectionner le canal sur lequel appliquer l\'effet, au choix parmi R,G,B,H,S,V ou ALL.'
+            hmess += '\nIl faut ensuite préciser le type de filtre à utiliser. Pour l\'instant, vous pouvez choisir parmi : DIFF-2D.'
+            hmess += '\nIl faut ensuite fournir les paramètres nécessaires au filtre, comme le rayon (entier).'
+            hmess += '\nEnfin, il faut renseigner la valeur de seuil (entier de 0 à 255).'
+            hmess += '\n\nExemple : detectEdges V DIFF-2D [5] 100'
+            hmess += '\n'
 
         elif matter == 'enhanceEdges':
-            hmess = '\nAIDE - ENHANCEEDGES'
+            hmess = 'AIDE - ENHANCEEDGES'
+            hmess += '\nCe module offre la possibilité d\'accetuer les contours sur une image.'
+            hmess += '\nCe module fonctionne de manière similaire au module HIGHPASS sauf qu\'il nécessite également une valeur de seuil et de gain (opacité).'
+            hmess += '\nIl faut d\'abord sélectionner le canal sur lequel appliquer l\'effet, au choix parmi R,G,B,H,S,V ou ALL.'
+            hmess += '\nIl faut ensuite préciser le type de filtre à utiliser. Pour l\'instant, vous pouvez choisir parmi : DIFF-2D.'
+            hmess += '\nIl faut ensuite fournir les paramètres nécessaires au filtre, comme le rayon (entier).'
+            hmess += '\nEnfin, il faut renseigner la valeur de seuil (entier de 0 à 255), ainsi que le gain (flottant de 0 à 1).'
+            hmess += '\n\nExemple : enhanceEdges V DIFF-2D [5] 100 .85'
+            hmess += '\n'
+
+
 
         elif matter == 'blackandwhite':
             hmess = '\nAIDE - BLACKANDWHITE'
+            hmess += '\nMalheureusement, cette fonction n\'est pas encore implémentée. Essayer d\'utiliser le module LEVELS sur le canal S.'
+
 
         elif matter == 'rotate':
             hmess = '\nAIDE - ROTATE'
+            hmess += '\nCe module offre la possibilité d\'appliquer une rotation d\'un angle (en degrés, flottant) donné sur une image.'
+            hmess += '\n\nExemple : rotate 30'
+            hmess += '\n'
 
         elif matter == 'crop':
             hmess = '\nAIDE - CROP'
+            hmess += '\nCe module offre la possibilité de recadrer une image.'
+            hmess += '\nIl suffit de préciser les coordonnées du nouveau point supérieur gauche et du nouveau point inférieur droit.'
+            hmess += '\n\nExemple : crop [10,20,150,210]'
+            hmess += '\n'
 
         elif matter == 'resize':
             hmess = '\nAIDE - RESIZE'
+            hmess += '\nCe module offre la possibilité de mettre à l\'échelle une image.'
+            hmess += ' Il suffit de préciser la largeur et la hauteur de la nouvelle image.'
+            hmess += '\n\nExemple : resize [800,600]'
+            hmess += '\n'
 
         elif matter == 'preview':
             hmess = '\nAIDE - PREVIEW'
+            hmess += '\nCe module ouvre une fenêtre pop-up afin de prévisualiser l\'image finale.'
+            hmess += '\n\nExemple : preview'
+            hmess += '\n'
 
         elif matter == 'history':
             hmess = '\nAIDE - HISTORY'
+            hmess += '\nMalheureusement, cette fonction n\'est pas encore totalement supportée.'
 
         elif matter == 'undo':
             hmess = '\nAIDE - UNDO'
+            hmess += '\nCette commande annule la dernière modification, jusqu\'à retourner à l\'état initial.'
+            hmess += '\n\nExemple : undo'
+            hmess += '\n'
 
         elif matter == 'redo':
             hmess = '\nAIDE - REDO'
+            hmess += '\nCette commande restaure la dernière action annulée par la commande UNDO. Si vous voulez restaurer plus d\'une action, la fonction essaiera de remonter le long de la branche de l\'historique jusqu\'à atteindre une feuille ou un noeud.'
+            hmess += '\n\nRemarque : Si un noeud est trouvé et que la commande REDO s\'avère inefficace, il est toujours possible de la forcer à entrer dans une branche ou à restaurer un état précis de l\'historique à l\'aide de la commande REBASE.'
+            hmess += '\n\nExemple : redo'
+            hmess += '\n'
 
         elif matter == 'rebase':
             hmess = '\nAIDE - REBASE'
+            hmess += '\nCette commande restaure le système à l\'état donné dans l\'historique courant.'
+            hmess += '\nIl suffit de préciser le numéro de l\'état à restaurer (0 pour l\'état initial).'
+            hmess += '\n\nExemple : rebase 0'
+            hmes += '\n'
 
         elif matter == 'open':
             hmess = '\nAIDE - OPEN'
+            hmess += '\nCette commande ouvre un fichier image compatible avec PhotoWizard.'
+            hmess += '\nIl suffit de préciser le chemin (relatif ou absolu) de l\'image.'
+            hmess += ' Ce chemin servira d\'identifiant pour sélectionner cette image parmi les différents fichiers ouverts.'
+            hmess += '\n\nExemple : open pic/test1.jpg'
+            hmess += '\n'
 
         elif matter == 'close':
             hmess = '\nAIDE - CLOSE'
+            hmess += '\nCette commande ferme une image donnée et supprime l\'historique correspondant (sauf s\'il a été sauvegardé dans un fichier XMD, bien sûr).'
+            hmess += '\nIl suffit de préciser l\'identifiant de votre image, qui est le chemin que vous avez entré pour l\'ouvrir.'
+            hmess += '\n\nExemple : close pic/test1.jpg'
+            hmess += '\n'
 
         elif matter == 'load':
             hmess = '\nAIDE - LOAD'
+            hmess += '\nCette commande charge un fichier XMD qui contient un historique de modifications, le greffe à l\'historique courant comme une nouvelle branche, et met à jour l\'état courant.'
+            hmess += '\nIl suffit de préciser le chemin vers le fichier XMD (relatif ou absolu).'
+            hmess += '\n\nExemple : load pic/test3.xmd'
+            hmess += '\n'
 
         elif matter == 'save':
             hmess = '\nAIDE - SAVE'
+            hmess += '\nCette commande sauvegarde l\'historique courant dans un fichier XMD, afin de permettre une restauration de celui-ci pour un usage futur.'
+            hmess += '\nIl suffit de préciser le chemin désiré (relatif ou absolu).'
+            hmess += '\n\nExemple : save pic/test3-1.xmd'
 
         elif matter == 'export':
             hmess = '\nAIDE - EXPORT'
+            hmess += '\nCette commande exporte l\'image finale. Toutes les modifications seront alors appliquées à votre image en pleine résolution.'
+            hmess += '\nIl suffit de préciser le chemin désiré (relatif ou absolu). L\'extension du fichier sera interprétée comme le format pour l\'export.'
+            hmess += '\n\nExemple : export pic/test3.tiff'
+            hmess += '\n'
 
         elif matter == 'switch':
             hmess = '\nAIDE - SWITCH'
+            hmess += '\nCette commande permet de changer entre plusieurs images ouvertes et de sélectionner l\'image courante de travail, permettant ainsi d\'éditer différentes images en parallèle.'
+            hmess += '\nIl suffit de préciser l\'identifiant de l\'image, qui est le chemin que vous avez entré pour l\'ouvrir.'
+            hmess += '\n\nExemple : switch pic/test2.jpg'
+            hmess += '\n'
 
         else:
             hmess = '\nAIDE - Sujet inconnu.'
